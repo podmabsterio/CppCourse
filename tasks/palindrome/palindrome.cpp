8bede0f0a0e0d1a6acc7865d3107333a2c6a8248
Boris Zhukov <unetriax@gmail.com>
2023-01-25 16:55:14 +0300

    First attempt palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,18 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    size_t j = str.size() - 1;
+    for (size_t i = 0; i < j;) {
+        if (str[i] == ' ') {
+            ++i;
+        } else if (str[j] == ' ') {
+            --j;
+        } else if (str[i] != str[j]) {
+            return false;
+        } else {
+            ++i;
+            --j;
+        }
+    }
+    return true;
 }
