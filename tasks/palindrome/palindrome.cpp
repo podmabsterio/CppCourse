2503ec22d6fef87b818fe773358c07e14af13d6b
Boris Zhukov <unetriax@gmail.com>
2023-01-25 22:07:47 +0300

    Code style fixed

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -5,8 +5,10 @@ bool IsPalindrome(const std::string& str) {
         return true;
     }
     for (size_t i = 0, j = str.size() - 1; i < j; ++i, --j) {
-        for (;i < j && str[i] == ' '; ++i) {}
-        for (;i < j && str[j] == ' '; --j) {}
+        for (;i < j && str[i] == ' '; ++i) {
+        }
+        for (;i < j && str[j] == ' '; --j) {
+        }
         if (str[i] != str[j]) {
             return false;
         }
