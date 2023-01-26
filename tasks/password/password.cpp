eb7f56c4c417dc8d0db1764e1a8671e83e3ab17d
Boris Zhukov <unetriax@gmail.com>
2023-01-26 12:20:53 +0300

    Types changed

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -11,22 +11,22 @@ bool ValidatePassword(const std::string& password) {
     if (password.size() < MIN_LENGTH || password.size() > MAX_LENGTH) {
         return false;
     }
-    int8_t has_upper_case = 0;
-    int8_t has_lower_case = 0;
-    int8_t has_digit = 0;
-    int8_t has_other = 0;
+    bool has_upper_case = false;
+    bool has_lower_case = false;
+    bool has_digit = false;
+    bool has_other = false;
     for (char ch : password) {
         if (ch > MAX_CHARACTER || ch < MIN_CHARACTER) {
             return false;
         } else if (std::islower(ch) != 0) {
-            has_lower_case = 1;
+            has_lower_case = true;
         } else if (std::isupper(ch) != 0) {
-            has_upper_case = 1;
+            has_upper_case = true;
         } else if (std::isdigit(ch) != 0) {
-            has_digit = 1;
+            has_digit = true;
         } else {
-            has_other = 1;
+            has_other = true;
         }
     }
-    return has_other + has_digit + has_lower_case + has_upper_case >= TYPES_REQUIRED;
+    return static_cast<int8_t>(has_other) + has_digit + has_lower_case + has_upper_case >= TYPES_REQUIRED;
 }
