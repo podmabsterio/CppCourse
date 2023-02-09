2bee4fb622afee246990aeb8026401a0e1fdd6bf
Boris Zhukov <unetriax@gmail.com>
2023-02-09 14:35:52 +0300

    First attempt

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,20 @@
 #include "tests_checking.h"
+#include <deque>
+
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> order;
+    for (auto& action : student_actions) {
+        if (action.side == Side::Bottom) {
+            order.emplace_back(action.name);
+        } else {
+            order.emplace_front(action.name);
+        }
+    }
+    std::vector<std::string> name_list;
+    for (auto place : queries) {
+        name_list.emplace_back(order.at(place - 1));
+    }
+    return name_list;
 }
