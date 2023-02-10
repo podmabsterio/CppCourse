1ccb1ac675287c17be4c5a5d600e5ceeff2ef40b
Boris Zhukov <unetriax@gmail.com>
2023-02-10 22:29:59 +0300

    what

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -11,16 +11,20 @@ struct TaskStatus {
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::unordered_map<std::string, std::unordered_map<std::string, TaskStatus> > table;
     for (auto& event : events) {
-        if (event.event_type == EventType::CheckFailed && event.time > table[event.student_name][event.task_name].cl_last_try && event.time <= score_time) {
+        if (event.event_type == EventType::CheckFailed &&
+            event.time > table[event.student_name][event.task_name].cl_last_try && event.time <= score_time) {
             table[event.student_name][event.task_name].cl_last_try = event.time;
             table[event.student_name][event.task_name].cl_success = false;
-        } else if (event.event_type == EventType::CheckSuccess && event.time > table[event.student_name][event.task_name].cl_last_try && event.time <= score_time) {
+        } else if (event.event_type == EventType::CheckSuccess &&
+                   event.time > table[event.student_name][event.task_name].cl_last_try && event.time <= score_time) {
             table[event.student_name][event.task_name].cl_last_try = event.time;
             table[event.student_name][event.task_name].cl_success = true;
-        } else if (event.event_type == EventType::MergeRequestClosed && event.time > table[event.student_name][event.task_name].merge_last_try && event.time <= score_time) {
+        } else if (event.event_type == EventType::MergeRequestClosed &&
+                   event.time > table[event.student_name][event.task_name].merge_last_try && event.time <= score_time) {
             table[event.student_name][event.task_name].merge_last_try = event.time;
             table[event.student_name][event.task_name].merge_closed = true;
-        } else if (event.event_type == EventType::MergeRequestOpen && event.time > table[event.student_name][event.task_name].merge_last_try && event.time <= score_time) {
+        } else if (event.event_type == EventType::MergeRequestOpen &&
+                   event.time > table[event.student_name][event.task_name].merge_last_try && event.time <= score_time) {
             table[event.student_name][event.task_name].merge_last_try = event.time;
             table[event.student_name][event.task_name].merge_closed = false;
         }
