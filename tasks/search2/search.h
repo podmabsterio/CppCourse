41abc32e7b80b8e119ceaf9c8a5bd73ed92226c8
Boris Zhukov <unetriax@gmail.com>
2023-02-20 01:21:10 +0300

    code style

--- tasks/search2/search.h
+++ tasks/search2/search.h
@@ -7,6 +7,7 @@ class SearchEngine {
 public:
     void BuildIndex(std::string_view text);
     std::vector<std::string_view> Search(std::string_view query, size_t results_count) const;
+
 private:
     std::vector<std::string_view> lines_;
 };
