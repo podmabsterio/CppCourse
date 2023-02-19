ad5356a6cc1529296b078120b868b2fb7f245e56
Boris Zhukov <unetriax@gmail.com>
2023-02-20 01:18:25 +0300

    first attempt

--- tasks/search2/search.h
+++ tasks/search2/search.h
@@ -7,4 +7,6 @@ class SearchEngine {
 public:
     void BuildIndex(std::string_view text);
     std::vector<std::string_view> Search(std::string_view query, size_t results_count) const;
+private:
+    std::vector<std::string_view> lines_;
 };
