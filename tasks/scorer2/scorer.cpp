27b64f8c2b0aa42ea4bebec611db8c5e5d2d2727
Boris Zhukov <unetriax@gmail.com>
2023-02-24 17:09:48 +0300

    First attempt

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -1,1 +1,46 @@
 #include "scorer.h"
+
+ScoreTable Scorer::GetScoreTable() const {
+    ScoreTable complete_table;
+    for (auto& [name, tasks] : table_) {
+        for (auto& [task_name, status] : tasks) {
+            if (status.merge_closed && status.cl_success) {
+                complete_table[name].insert(task_name);
+            }
+        }
+    }
+    return complete_table;
+}
+
+void Scorer::Cleaner(const StudentName& student_name, const TaskName& task_name) {
+    if (!table_[student_name][task_name].cl_success && table_[student_name][task_name].merge_closed) {
+        table_[student_name].erase(task_name);
+    }
+    if (table_[student_name].empty()) {
+        table_.erase(student_name);
+    }
+}
+
+void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
+    table_[student_name][task_name].cl_success = false;
+    this->Cleaner(student_name, task_name);
+}
+
+void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
+    table_[student_name][task_name].cl_success = true;
+}
+
+void Scorer::OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
+    table_[student_name][task_name].merge_closed = false;
+}
+
+void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
+    table_[student_name][task_name].merge_closed = true;
+    this->Cleaner(student_name, task_name);
+}
+
+void Scorer::Reset() {
+    table_.clear();
+}
+
+
